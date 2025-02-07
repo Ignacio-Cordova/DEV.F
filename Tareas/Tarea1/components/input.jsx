566709e45820a6b37263ec4958367b84e62de5a7
Ignacio-Cordova <ignaciojcm2005@gmail.com>
2025-02-06 23:40:02 -0300

    Add files via upload

---
+++ Tareas/Tarea1/components/input.jsx
@@ -0,0 +1,9 @@
+const Input = (props) => {  
+    return <input 
+    id={props.id} 
+    type={props.type}
+    name={props.id} 
+    className={props.className} 
+    placeholder={props.placeholder}></input>
+}
+export default Input;
