2c89e0b0084149fb5bf1ad5ee3e6b0cac85ede1d
Ignacio-Cordova <ignaciojcm2005@gmail.com>
2025-02-06 23:42:42 -0300

    Delete Tareas/Tarea1 directory

--- Tareas/Tarea1/components/input.jsx
+++
@@ -1,9 +0,0 @@
-const Input = (props) => {  
-    return <input 
-    id={props.id} 
-    type={props.type}
-    name={props.id} 
-    className={props.className} 
-    placeholder={props.placeholder}></input>
-}
-export default Input;
